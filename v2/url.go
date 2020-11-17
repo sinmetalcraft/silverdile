fb407572845a6f704b59e53807d1262be910f588
sinmetal <sinmetal@sinmetalcraft.jp>
2020-11-17 14:58:10 +0900

    object に "/" が含まれているケースに対応した

--- v2/url.go
+++ v2/url.go
@@ -26,34 +26,50 @@ type ImageOption struct {
 // `/{bucket}/{object}`
 // `/{bucket}/{object}/=sXXX`
 func BuildImageOption(path string) (*ImageOption, error) {
-	var ret ImageOption
+	ret := &ImageOption{}
 
 	blocks := strings.Split(path, "/")
 	if len(blocks) < 3 {
 		return nil, NewErrInvalidArgument("Fewer expected blocks separated by `/`", map[string]interface{}{}, nil)
 	}
 	ret.Bucket = blocks[1]
-	ret.Object = blocks[2]
+	blocks = blocks[2:len(blocks)]
 
-	// resize 指定がない場合は、そこで終わり
-	if len(blocks) < 4 {
-		return &ret, nil
+	size, err := buildSize(path)
+	if err != nil {
+		return nil, err
 	}
-
-	l := sizeRegexp.FindAllStringSubmatch(path, -1)
-	if len(l) > 0 {
-		v := l[len(l)-1]
-		vv := v[0]
-		size, err := strconv.Atoi(vv[2:])
-		if err != nil {
-			return nil, err
-		}
-		if size < MinResizeSize || size > MaxResizeSize {
-			return nil, NewErrInvalidArgument(fmt.Sprintf("invalid resize arugment. size range is %d ~ %d, but got %d", MinResizeSize, MaxResizeSize, size), map[string]interface{}{}, nil)
-		}
+	if size > 0 {
 		ret.Size = size
-		return &ret, nil
+		blocks = blocks[:len(blocks)-1] // 末尾は Size で消費されたので残りの部分が Object Path
+	}
+	ret.Object = strings.Join(blocks, "/")
+
+	if len(ret.Bucket) < 1 || len(ret.Object) < 1 {
+		return nil, NewErrInvalidArgument("invalid path", map[string]interface{}{"path": path}, nil)
 	}
 
-	return nil, NewErrInvalidArgument("invalid path", map[string]interface{}{"path": path}, nil)
+	return ret, nil
+}
+
+func buildSize(path string) (int, error) {
+	l := sizeRegexp.FindAllStringSubmatch(path, -1)
+	if len(l) < 1 {
+		return 0, nil
+	}
+
+	v := l[len(l)-1]
+	vv := v[0]
+	size, err := strconv.Atoi(vv[2:])
+	if err != nil {
+		return 0, NewErrInvalidArgument(
+			fmt.Sprintf("invalid resize arugment. size range is %d ~ %d, but got %d", MinResizeSize, MaxResizeSize, size),
+			map[string]interface{}{}, err)
+	}
+	if size < MinResizeSize || size > MaxResizeSize {
+		return 0, NewErrInvalidArgument(
+			fmt.Sprintf("invalid resize arugment. size range is %d ~ %d, but got %d", MinResizeSize, MaxResizeSize, size),
+			map[string]interface{}{}, nil)
+	}
+	return size, nil
 }
